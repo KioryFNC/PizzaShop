d7bcc83c97ef46b844d8710d43c834f19a6bcc49
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-04-09 12:22:28 -0300

    feat restaurant registration

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
+
 import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
+
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 
 export const router = createBrowserRouter([
@@ -15,6 +19,10 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element:<AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> },]
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> }
+
+    ]
   }
 ])
